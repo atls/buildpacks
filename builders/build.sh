b1324771b8e067ba87e9537d6c174f89b55d0780
Nikita Elfimov <nelfimov@mail.ru>
2024-12-19 18:30:26 +0300

    fix(stacks): user and platforms (#30)

--- builders/build.sh
+++ builders/build.sh
@@ -3,4 +3,4 @@ set -e
 
 BUILDER_DIR="${1:-base}"
 
-pack builder create "atlantislab/builder-${BUILDER_DIR}:22" --verbose --config "./${BUILDER_DIR}/builder.toml"
+pack builder create "atlantislab/builder-${BUILDER_DIR}:22" --verbose --config "./${BUILDER_DIR}/builder.toml" --publish
