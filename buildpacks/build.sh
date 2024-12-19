1436251f6f937ddf2dc45733ae67b89c86c556f5
Nikita Elfimov <nelfimov@mail.ru>
2024-12-19 17:20:52 +0300

    feat(buildpacks): update, support arm64

--- buildpacks/build.sh
+++ buildpacks/build.sh
@@ -4,18 +4,12 @@ set -e
 cwd=$PWD
 
 yarn workspace @atls/buildpack-yarn-install build
-cd "${cwd}/yarn-install" && pack buildpack package atlantislab/buildpack-yarn-install:0.1.0 --config ./package.toml
+cd "${cwd}/yarn-install" && pack buildpack package atlantislab/buildpack-yarn-install:0.1.1 --config ./package.toml --publish
 
 yarn workspace @atls/buildpack-yarn-cache build
-cd "${cwd}/yarn-cache" && pack buildpack package atlantislab/buildpack-yarn-cache:0.1.0 --config ./package.toml
+cd "${cwd}/yarn-cache" && pack buildpack package atlantislab/buildpack-yarn-cache:0.1.1 --config ./package.toml --publish
 
 yarn workspace @atls/buildpack-yarn-workspace-start build
-cd "${cwd}/yarn-workspace-start" && pack buildpack package atlantislab/buildpack-yarn-workspace-start:0.1.0 --config ./package.toml
+cd "${cwd}/yarn-workspace-start" && pack buildpack package atlantislab/buildpack-yarn-workspace-start:0.1.1 --config ./package.toml --publish
 
-docker push atlantislab/buildpack-yarn-install:0.1.0
-docker push atlantislab/buildpack-yarn-cache:0.1.0
-docker push atlantislab/buildpack-yarn-workspace-start:0.1.0
-
-cd "${cwd}/yarn-workspace" && pack buildpack package atlantislab/buildpack-yarn-workspace:0.1.0 --config ./package.toml
-
-docker push atlantislab/buildpack-yarn-workspace:0.1.0
+cd "${cwd}/yarn-workspace" && pack buildpack package atlantislab/buildpack-yarn-workspace:0.1.1 --config ./package.toml --target 'linux/amd64' --target 'linux/arm64' --publish
