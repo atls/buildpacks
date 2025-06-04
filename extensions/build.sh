8b3849d70066974b0e4b19a46d67185d1ab08a37
Nikita Elfimov <nelfimov@mail.ru>
2025-06-04 17:53:44 +0300

    feat(common): hive extension (#35)

--- extensions/build.sh
+++ extensions/build.sh
@@ -5,3 +5,4 @@ cwd=$PWD
 
 cd "${cwd}/curl" && pack extension package atlantislab/buildpack-extension-curl:0.0.1 --publish
 cd "${cwd}/htop" && pack extension package atlantislab/buildpack-extension-htop:0.0.1 --publish
+cd "${cwd}/graphql-hive" && pack extension package atlantislab/buildpack-extension-graphql-hive:0.0.1 --publish
